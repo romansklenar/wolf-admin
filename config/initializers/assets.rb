f2d8079585a48d8e430a8aa84a551b773a9d8a5a
Roman Sklenář <mail@romansklenar.cz>
2015-04-16 16:11:28 +0200

    ActiveAdmin: precompile assets on production
    # Conflicts:
    #	config/initializers/assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( mailer.css )
+Rails.application.config.assets.precompile += %w( active_admin.css active_admin.js active_admin/print.css )
