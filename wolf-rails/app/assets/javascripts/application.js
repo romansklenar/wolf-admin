32650993bc2baf70fe5ecc0a929c24dbc5615f56
Roman Sklenář <mail@romansklenar.cz>
2015-07-21 14:25:36 +0200

    Remove Bootstrap from assets and use gem version

--- wolf-rails/app/assets/javascripts/application.js
+++ wolf-rails/app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require bootstrap/bootstrap.min
+//= require bootstrap/bootstrap
 //= require ../../../vendor/assets/javascripts/jquery.flot/jquery.flot
 //= require ../../../vendor/assets/javascripts/jquery.flot/jquery.flot.time
 //= require ../../../vendor/assets/javascripts/jquery.flot/jquery.flot.categories
