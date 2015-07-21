76520fc2d8a0a2320878bebc53c5536a9e485711
Roman Sklenář <mail@romansklenar.cz>
2015-07-21 17:31:26 +0200

    Remove Bootstrap datepicker from assets and use gem version

--- wolf-rails/app/assets/javascripts/application.js
+++ wolf-rails/app/assets/javascripts/application.js
@@ -15,6 +15,7 @@
 //= require turbolinks
 //= require moment
 //= require bootstrap
+//= require bootstrap-datepicker
 //= require bootstrap-daterangepicker
 //= require select2
 //= require jquery-flot
