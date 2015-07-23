b8174813c26d71df78257e0826786c53300573f8
Roman Sklenář <mail@romansklenar.cz>
2015-07-23 15:03:21 +0200

    Prefer Sass and CoffeeScript over vanilla CSS & JS

--- app/assets/javascripts/jquery-flot.js
+++
@@ -1,4 +0,0 @@
-//= require jquery-flot/jquery.flot
-//= require jquery-flot/jquery.flot.time
-//= require jquery-flot/jquery.flot.categories
-//= require flot.tooltip
---
+++ app/assets/javascripts/jquery-flot.js.coffee
@@ -0,0 +1,4 @@
+#= require jquery-flot/jquery.flot
+#= require jquery-flot/jquery.flot.time
+#= require jquery-flot/jquery.flot.categories
+#= require flot.tooltip
