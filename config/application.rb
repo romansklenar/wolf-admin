0f7b5fb02807eb708254ae49d89fef2883388ebc
Roman Sklenář <mail@romansklenar.cz>
2015-08-13 22:33:28 +0200

    Updated Rails config files to latest version
    command:
    rake rails:update

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,9 @@ module WolfRails
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
+    
     # Use custom exception handler
     config.exceptions_app = self.routes
   end
