3615ba67111b46cb04c83f1a0703f4eff223d0da
Roman Sklenář <mail@romansklenar.cz>
2015-08-13 10:10:02 +0200

    Added simple error handling for different kind of responses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :detect_pjax
-
-  private
-    def detect_pjax
-      if request.headers['X-PJAX']
-        self.class.layout "pjax"
-      else
-        self.class.layout "application"
-      end
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.html { redirect_to backlink, alert: exception.message }
+      format.js   { render partial: 'shared/access_denied', locals: { exception: exception } }
+      format.json { render json: { error: exception.message }, status: 403 }
     end
+  end if defined?(CanCan)
+
+  protected
+
+  def backlink
+    user_signed_in? ? after_sign_in_path_for(current_user) : (request.referer || root_path)
+  end
 end
