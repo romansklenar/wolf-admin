cb96b4bd0f35b5bc0ef5ffb65cebc59ff2bcceca
Roman Sklenář <mail@romansklenar.cz>
2015-08-17 22:55:45 +0200

    Added ActiveAdmin for nice admin interface
    rails g active_admin:install Administrator --skip-users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  ActiveAdmin.routes(self)
   devise_for :users, controllers: {
     confirmations:      "users/devise/confirmations",
     # omniauth_callbacks: "users/devise/omniauth_callbacks",
