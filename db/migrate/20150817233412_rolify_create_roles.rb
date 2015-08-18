a1e958ef0d79e85ce73f97117e4c2a0fafda43c0
Roman Sklenář <mail@romansklenar.cz>
2015-08-18 22:45:35 +0200

    Fixed timestamps depracation warning
    DEPRECATION WARNING: `#timestamps` was called without specifying an option for `null`. In Rails 5, this behavior will change to `null: false`. You should manually specify `null: true` to prevent the behavior of your existing migrations from changing.

--- db/migrate/20150817233412_rolify_create_roles.rb
+++ db/migrate/20150817233412_rolify_create_roles.rb
@@ -3,14 +3,14 @@ class RolifyCreateRoles < ActiveRecord::Migration
     create_table :roles do |t|
       t.string :name, index: true
       t.references :resource, polymorphic: true
-      t.timestamps
+      t.timestamps null: false
 
       t.index [:name, :resource_type, :resource_id]
     end
 
     create_join_table :users, :roles, table_name: 'users_roles' do |t|
       t.index [:user_id, :role_id], unique: true
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
